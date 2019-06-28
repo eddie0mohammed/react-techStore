f088f04ca507edb7e9d64137493f7bfa0e3b6b69
eddie0mohammed <me3dun@gmail.com>
2019-06-28 08:35:10 -0700

    product component

--- src/components/HomePage/Featured.js
+++ src/components/HomePage/Featured.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import Product from '../Product';
+import {Link} from 'react-router-dom';
+import Title from '../Title';
+import {ProductConsumer } from '../../context/context';
 
 const Featured = () => {
     return (
-        <div>
-            Featured
-        </div>
+        <section className="py-5">
+            <div className="container">
+                <Title title="featured products" center="true" />
+                <div className="row">
+                    <ProductConsumer>
+                        {(value) => {
+                            const {featuredProducts} = value;
+                            return (
+                                featuredProducts.map(product => (<Product key={product.id} product={product}></Product>) )
+
+                            )
+                        }}
+
+                    </ProductConsumer>
+                </div>
+            </div>
+        </section>
     )
 }
 
--- src/context/context.js
+++ src/context/context.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {linkData} from './linkData';
 import {socialData} from './socialData';
+import {items} from './productData';
 
 const ProductContext = React.createContext();
 
@@ -9,12 +10,22 @@ export class ProductProvider extends Component {
     state = {
         sideBarOpen: false,
         cartOpen: false,
-        cartItems: 0,
         links: linkData,
         socialIcons: socialData,
         cart: [],
+        cartItems: 0,
+        cartSubTotal: 0,
+        cartTax: 0,
+        cartTotal: 0,
+        storeProducts: [],
+        filteredProducts: [],
+        featuredProducts: [],
+        singleProduct: {},
+        loading: false,
         
-        
+    }
+    componentDidMount(){
+        this.setProducts(items);
     }
 
     handleSidebar = () => {
@@ -29,6 +40,52 @@ export class ProductProvider extends Component {
     openCart = () => {
         this.setState({cartOpen: true});
     }
+    //get cart from storage
+    getStorageCart = () => {
+        return [];
+    }
+    //get product from local storage
+    getStorageProduct = () => {
+        return [];
+    }
+    //get totals
+    getTotals = () => {};
+
+    //add totals
+    addTotals = () => {};
+
+    //sync storage
+    syncStorage = () => {};
+
+    //add to cart
+    addToCart = (id) => {
+        console.log(`add to cart ${id}`);
+    }
+    //set single product
+    setSingleProduct = (id) => {
+        console.log(`set single product ${id}`);
+    }
+
+    //set products
+    setProducts = (products) => {
+        let storeProducts = products.map(item => {
+            const {id} = item.sys;
+            const image = item.fields.image.fields.file.url;
+            const product = {id, ...item.fields, image};
+            return product;
+        })
+         //featured products
+        let featuredProducts = storeProducts.filter(item => item.featured === true);
+        this.setState({
+            storeProducts,
+            filteredProducts: storeProducts,
+            featuredProducts,
+            cart: this.getStorageCart(),
+            singleProduct: this.getStorageProduct(),
+            loading: false 
+        });
+    } 
+   
 
 
     render() {
@@ -38,7 +95,9 @@ export class ProductProvider extends Component {
                 handleSidebar: this.handleSidebar,
                 handleCart: this.handleCart,
                 closeCart: this.closeCart,
-                openCart: this.openCart
+                openCart: this.openCart,
+                addToCart: this.addToCart,
+                setSingleProduct: this.setSingleProduct,
             }}>
                 {this.props.children}
             </ProductContext.Provider>
